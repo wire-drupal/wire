d51f720c8b5467c467c21ef3d9ac66ba5d95e742
cornel <hugronaphor@gmail.com>
2024-09-27 12:41:12 +0300

    Don't depend on server detecting url scheme.

--- js/util/getAppUrl.js
+++ js/util/getAppUrl.js
@@ -1,9 +1,3 @@
 export function getAppUrl() {
-    const appUrlTag = document.head.querySelector('meta[name="w-app-url"]')
-
-    if (appUrlTag) {
-        return appUrlTag.content
-    }
-
     return window.wire_app_url ?? window.location.origin ?? ''
 }
