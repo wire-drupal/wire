c3c60b7a04b929ae2f406102b649908f54ac2a5a
cornel <hugronaphor@gmail.com>
2024-10-22 23:59:24 +0300

    Remove breaking change.

--- src/WireComponentBase.php
+++ src/WireComponentBase.php
@@ -44,7 +44,7 @@ class WireComponentBase extends PluginBase implements WireComponentInterface, Co
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration,
       $plugin_id,
